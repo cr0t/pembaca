3e220396cdae0a36390442962162963d976e3436
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2010-08-29 22:07:45 +0400

    TYPO fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
   # :token_authenticatable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :uauthable
+         :confirmable, :oauthable
   
   references_many :uploads, :dependent => :delete
 end
