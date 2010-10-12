f44ab110386bf3faf95003ed74aec9edd55ca405
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2010-10-12 16:13:03 +0400

    Maked some fixes to the main page and styles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @uploads = Upload.find(:all, :conditions => { :public => true, :converted => true }).descending(:created_at)
+    if user_signed_in?
+      @public_books = Upload.find(:all, :conditions => { :public => true, :converted => true, :user_id.ne => current_user.id }).descending(:created_at)
+      @own_books    = current_user.uploads.where({ :converted => true }).descending(:created_at)
+    else
+      @public_books = Upload.find(:all, :conditions => { :public => true, :converted => true}).descending(:created_at)
+    end
   end
   
   def terms
