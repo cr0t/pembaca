9161611ad56426ca680c706e58d502711b0f6ee6
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2010-11-24 10:22:59 +0300

    Fixed production cofnig and addded some rescues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Pembaca::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # Devise need it
+  config.action_mailer.default_url_options = { :host => 'pembaca.summercode.com' }
 end
