3cf51c21f35983f6c5890bc78f5e56c32172d5cc
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-03-18 18:10:03 +0300

    Updated deploy.rb config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,4 +19,8 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  
+  after "deploy:update" do
+    deploy::cleanup
+  end
 end
