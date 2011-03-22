1d84c335ce25288879a33c81150bb7e2087b3e5a
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-03-22 20:08:24 +0300

    Reduced number of unicorn workers from 4 to 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,7 +12,7 @@ APP_PATH = "/var/www/com.summercode.pembaca"
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 4
+worker_processes 2
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
