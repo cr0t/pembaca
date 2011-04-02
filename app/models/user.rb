3dfdec734c57390758458fbacd13cc57e2908e24
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-04-02 21:02:22 +0400

    Fixed User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,27 +10,15 @@ class User
   
   def self.find_for_facebook_oauth(access_token, signed_in_resource = nil)
     data = access_token['extra']['user_hash']
-    if user = User.find_by_email(data["email"])
+    if user = User.first(:conditions => { :email => data["email"] })
       user
     else # Create an user with a stub password.
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0, 20])
+      User.create!(
+        :name         => data["name"],
+        :email        => data["email"],
+        :confirmed_at => Time.now,
+        :facebook_id  => data["id"],
+        :password     => Devise.friendly_token)
     end
   end
-  
-#  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-#      # Get the user email info from Facebook for sign up
-#      data = ActiveSupport::JSON.decode(access_token.get('/me'))
-#
-#      if user = User.first(:conditions => { :email => data["email"] })
-#        user
-#      else
-#        # Create an user with a stub password.
-#        User.create!(
-#          :name         => data["name"],
-#          :email        => data["email"],
-#          :confirmed_at => Time.now,
-#          :facebook_id  => data["id"],
-#          :password     => Devise.friendly_token)
-#      end
-#    end
 end
