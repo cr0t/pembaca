eded08d68656e076dcd323a93c9e268d5b7fe371
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-04-02 21:52:51 +0400

    Updated sign in authentication for Facebook APP

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -2,6 +2,13 @@ class CanvasController < ApplicationController
   layout "facebook"
   
   def index
+    unless params[:fb_sig_user].nil?
+      @user = User.first(:where => "facebook_id == ?", params[:fb_sig_user])
+      if @user.persisted?
+        sign_in @user
+      end
+    end
+    
     @uploads = Upload.find(:all, :conditions => { :public => true, :converted => true }).descending(:created_at)
   end
   
