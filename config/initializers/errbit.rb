d587109d19f20932351c4c76ef16bf4afdb7d988
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-06-16 12:57:36 +0400

    Added errbit initializer file

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'ad744ce9befdf6f306e3977ee2724de6'
+  config.host    = 'errbit.summercode.com'
+  config.port    = 80
+  config.secure  = config.port == 443
+end
