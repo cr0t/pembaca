919ad4d82de01450b90dcd83c3ceeb45adafbfdd
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-12-12 00:59:50 +0400

    Fixed NoMethodError: undefined method 'total_pages' for nil:NilClass

--- app/controllers/simple_viewer_controller.rb
+++ app/controllers/simple_viewer_controller.rb
@@ -5,7 +5,8 @@ class SimpleViewerController < ApplicationController
     cookies.permanent[:last_book_id]  = params[:id]
     cookies.permanent[:last_page_num] = params[:page]
     
-    @book = Upload.first(:conditions => {:_id=>BSON::ObjectId(params[:id])})
+    @book = Upload.first(:conditions => { :_id => BSON::ObjectId(params[:id]) })
+    redirect_to root_path, :notice => "We can not find book for the given id" if @book.nil?
     @page = params[:page].rjust(6, "0")
   end
   
@@ -13,7 +14,7 @@ class SimpleViewerController < ApplicationController
     if cookies[:last_book_id].nil?
       redirect_to root_path, :notice => "You haven't read any book before"
     else
-      @book = Upload.first(:conditions => {:_id=>BSON::ObjectId(cookies[:last_book_id])})
+      @book = Upload.first(:conditions => { :_id => BSON::ObjectId(cookies[:last_book_id]) })
       @page = cookies[:last_page_num].rjust(6, "0")
 
       render :view
